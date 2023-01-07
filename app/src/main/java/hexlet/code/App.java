2beda80a861b7d7510253bb32f4c7df1c99f067e
Artem V.R <artra@ya.ru>
2023-01-07 17:31:20 +0300

    0107-1 step 1: ready to deploy

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,31 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import io.javalin.core.JavalinConfig;
+
+/*
+
+ */
+public class App {
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
+    }
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(JavalinConfig::enableDevLogging); //with logging
+        addRoutes(app); //adding routing
+        return app;
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", RootController.welcome);
+    }
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "8080");
+        return Integer.valueOf(port);
+    }
+
+}
+
---
+++ app/src/main/java/hexlet/code/RootController.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import io.javalin.http.Handler;
+
+import java.io.PrintWriter;
+
+public class RootController {
+    public static Handler welcome = ctx -> {
+        PrintWriter pw = ctx.res.getWriter();
+        pw.write("Hello, Web");
+    };
+}

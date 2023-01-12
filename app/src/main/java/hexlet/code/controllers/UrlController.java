4ad50cf93fc15ac64f9f139c43a0c9c04237019e
Artem V.R <artra@ya.ru>
2023-01-12 22:43:24 +0300

    0112-1 step 3

--- app/src/main/java/hexlet/code/controllers/UrlController.java
+++ app/src/main/java/hexlet/code/controllers/UrlController.java
@@ -5,15 +5,19 @@ import io.ebean.EbeanServer;
 import io.javalin.http.Handler;
 
 import java.io.PrintWriter;
+import java.net.URL;
 
 public class UrlController {
 
     public static Handler newurl = ctx -> {
-        String url = ctx.bodyAsClass(String.class);
+        String urlString = ctx.formParam("url");
+        URL url = new URL(urlString);
+        URL toAdd = new URL(url.getProtocol(), url.getHost(), url.getPort(), "");
+
         //EbeanServer ebeanServer = DBSererInstance.getInstance();
 
         PrintWriter printWriter = ctx.res.getWriter(); //res - response
-        printWriter.write(url + "added");
+        printWriter.write(toAdd + " added");
 
     };
 }

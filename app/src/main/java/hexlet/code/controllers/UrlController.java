031250c4d6b1c69b4ce5e98b8aa8e0cb40ce4e41
Artem V.R <artra@ya.ru>
2023-01-13 07:46:29 +0300

    0113-1 step 3

--- app/src/main/java/hexlet/code/controllers/UrlController.java
+++ app/src/main/java/hexlet/code/controllers/UrlController.java
@@ -12,8 +12,13 @@ public class UrlController {
     public static Handler newurl = ctx -> {
         String urlString = ctx.formParam("url");
         URL url = new URL(urlString);
-        URL toAdd = new URL(url.getProtocol(), url.getHost(), url.getPort(), "");
-
+        URL toAdd = null;
+        try {
+            toAdd = new URL(url.getProtocol(), url.getHost(), url.getPort(), "");
+        } catch (Exception e) {
+            PrintWriter printWriter = ctx.res.getWriter(); //res - response
+            printWriter.write(urlString + " is incorrect");    
+        }
         //EbeanServer ebeanServer = DBSererInstance.getInstance();
 
         PrintWriter printWriter = ctx.res.getWriter(); //res - response
